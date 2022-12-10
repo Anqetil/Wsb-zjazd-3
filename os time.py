7535f02adee24539ea3e2e38f97bb9bf9fb913dd
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 13:06:15 +0100

    pythoncwiczenia

--- os time.py
+++ os time.py
@@ -1,9 +1,10 @@
-print("a")
 import os
 import time
-print (os.getcwd())
-os.mkdir('python1')
-time.sleep(5)
-#os.rename('python1', 'python2')
-#time.sleep(4)
-#os.rmdir('python2')
+my_string1 = 'mom'
+my_string2 = 'dad'
+sum1 = my_string1 + my_string2
+print(sum1)
+sum2 = 'my ' + my_string1  + ' and my ' + my_string2
+print(sum2)
+print(sum2[3:6])
+print(sum2[-4: -1])
