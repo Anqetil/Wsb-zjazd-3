d0fca940de007100c3c615004e8b7a61f645789c
anqetil <g.sob78@gmail.com>
2023-03-29 16:24:57 +0200

    aki zmianaq

--- os time.py
+++ os time.py
@@ -1,10 +1,30 @@
-import os
-import time
-my_string1 = 'mom'
-my_string2 = 'dad'
-sum1 = my_string1 + my_string2
-print(sum1)
-sum2 = 'my ' + my_string1  + ' and my ' + my_string2
-print(sum2)
-print(sum2[3:6])
-print(sum2[-4: -1])
+print('to jest moj pierwszy program')
+nick = 'trener pytnon'
+print(nick)
+print("2+2 =", 2+2)
+print("2-2 =", 2-2)
+print("2/2 =", 2/2)
+print("2*2 =", 2*2)
+print("20**2 =", 20**2)
+# print("100//2 =", 100//2)
+motto = "motto: \ 'siem ma'"
+print(motto)
+wielowierszowe_motto = "'dalej marze zeby sie przywitac'"
+print(wielowierszowe_motto)
+#print("%s %s %s" \ ('lubie', motto, wielowierszowe_motto ))
+
+lista_zakupów = ('jajka', 'pomidory', 'czili', 'ser')
+print('najważniejszy składnik jajaczniczy to ->', lista_zakupów[0])
+print('najważniejszy składnik jajaczniczy to ->', lista_zakupów[2])
+# lista_zakupów[0] = ["warzywa na patelnie"]
+# print(lista_zakupów)
+#momenty
+print(lista_zakupów[1:3])
+#inne czynnosci
+inne_czynnosci = ('ogladanie tv', 'programowanie','sranie')
+do_zrobienia = [inne_czynnosci, lista_zakupów]
+print(do_zrobienia)
+print(do_zrobienia[1][1])
+#dodaj wartosci za pomoca appentd('cytryna')
+lista_zakupów.('cytrya')
+print(do_zrobienia)
